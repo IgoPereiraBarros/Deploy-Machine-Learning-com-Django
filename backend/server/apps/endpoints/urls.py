671aa45857f3cb78b1f45e42cd2084b6345a1db2
IgoPereiraBarros <igorestacioceut@gmail.com>
2019-11-08 14:36:13 -0300

    Criando API Rest para os modelos

---
+++ backend/server/apps/endpoints/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from .views import EndPointViewSet
+from .views import MLAlgorithmViewSet
+from .views import MLAlgorithmStatusViewSet
+from .views import MLRequestViewSet
+
+
+router = DefaultRouter(trailing_slash=False)
+router.register('endpoints', EndPointViewSet, basename='endpoints')
+router.register('mlalgorithms', MLAlgorithmViewSet, basename='mlalgorithms')
+router.register('mlalgorithmstatuses', MLAlgorithmStatusViewSet, basename='mlalgorithmstatuses')
+router.register('mlrequests', MLRequestViewSet, basename='mlrequests')
+
+urlpatterns = [
+    url('^api/v1/', include(router.urls)),
+]
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    #myapps
+    'apps.endpoints'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,15 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'deploy_ML',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
